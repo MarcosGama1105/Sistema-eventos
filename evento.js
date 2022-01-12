18fb454e4220e2dc6746d7f657f19b2255035950
marcosgama1105 <markytoremo@gmail.com>
2022-01-12 18:16:06 -0300

    SistemaEventos

---
+++ evento.js
@@ -0,0 +1,25 @@
+var listaDoevento = ["participante , palestrante"]
+if (listaDoevento.length < 100) {
+    //é possível cadastrar
+    console.log("é possível cadastrar participantes")
+}else {
+    console.log("excede o limite permitido")
+}
+
+
+let idade = 18;
+if(idade>18){
+//participante maior que dezoito
+    console.log("participante tem que ser maior de dezoito")
+}else {
+    console.log("participante possui idade inferior")
+}
+
+
+let dataDoevento = 30;
+if(data=30){
+//permitir cadastro evento    
+    console.log("permitir cadastro evento")
+}else {
+    console.log("cadastro não permitido")
+}
